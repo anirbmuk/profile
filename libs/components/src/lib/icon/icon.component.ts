e9243ec2d7f2de93c457b0bd62e204fb5fe69b35
anirbmuk <anirbmuk@live.in>
2022-05-31 06:53:45 +0200

    added ratings component and improved icons

--- libs/components/src/lib/icon/icon.component.ts
+++ libs/components/src/lib/icon/icon.component.ts
@@ -18,13 +18,13 @@ export class IconComponent {
   }
 
   @Input() set size(value: number | null | undefined) {
-    const useValue = value ?? 8;
+    const useValue = value ?? 4;
     this.iconClass = `w-${useValue} h-${useValue}`;
   }
 
   type: 'link' | 'text' = 'text';
   ariaLabel?: string;
-  iconClass = 'w-8 h-8';
+  iconClass = 'w-4 h-4';
   source?: string | undefined;
   target?: string | undefined;
 }
