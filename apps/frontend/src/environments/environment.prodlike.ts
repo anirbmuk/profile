3987b322907743706a915a128fc34dc803f02381
anirbmuk <anirbmuk@live.in>
2022-06-11 18:45:09 +0200

    added env configs for build

--- apps/frontend/src/environments/environment.prodlike.ts
+++ apps/frontend/src/environments/environment.prodlike.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  hostUrl: 'https://anirbmuk.appspot.com',
+  hostUrl: 'http://localhost:4300',
   config: null,
   google: {
     verified: false,
