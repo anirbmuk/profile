5028bb8c4a91814372b4f01f633945e020236c2a
anirbmuk <anirbmuk@live.in>
2022-06-23 23:45:17 +0200

    fix: mobile display improvements

--- libs/components/src/lib/icon/icon.component.ts
+++ libs/components/src/lib/icon/icon.component.ts
@@ -38,24 +38,24 @@ export class IconComponent {
 
   type: 'link' | 'text' = 'text';
   ariaLabel?: string;
-  iconClass = 'w-4 h-4';
+  iconClass = 'h-3 md:h-4 w-3 md:w-4';
   source?: string | undefined;
   target?: string | undefined;
   displayClass?: string;
 
   sizeMappers: Record<number, string> = {
-    2: 'h-2 w-2',
-    3: 'h-3 w-3',
-    4: 'h-4 w-4',
-    5: 'h-5 w-5',
-    6: 'h-6 w-6',
-    7: 'h-7 w-7',
-    8: 'h-8 w-8',
-    9: 'h-9 w-9',
-    10: 'h-10 w-10',
-    12: 'h-12 w-12',
-    14: 'h-14 w-14',
-    16: 'h-16 w-16',
+    2: 'h-1 md:h-2 w-1 md:w-2',
+    3: 'h-2 md:h-3 w-2 md:w-3',
+    4: 'h-3 md:h-4 w-3 md:w-4',
+    5: 'h-4 md:h-5 w-4 md: w-5',
+    6: 'h-5 md:h-6 w-5 md:w-6',
+    7: 'h-6 md:h-7 w-6 md:w-7',
+    8: 'h-7 md:h-8 w-7 md:w-8',
+    9: 'h-8 md:h-9 w-8 md:w-9',
+    10: 'h-9 md:h-10 w-9 md:w-10',
+    12: 'h-11 md:h-12 w-11 md:w-12',
+    14: 'h-13 md:h-14 w-13 md:w-14',
+    16: 'h-15 md:h-16 w-15 md:w-16',
   };
 
   displayClasses: Record<'start' | 'middle' | 'end', string> = {
