1234fc68074500ca60e59bd13bdab35a5fdb7cff
anirbmuk <anirbmuk@live.in>
2022-06-24 22:58:36 +0200

    fix: added swupdate subscription for pwa updates

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { isPlatformBrowser } from '@angular/common';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
@@ -6,8 +6,10 @@ import {
   PLATFORM_ID,
   Renderer2,
 } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 import { TrackingService } from '@frontend/components';
 import { FOOTER, IFooterBlock } from '@frontend/connector-interfaces';
+import { take } from 'rxjs/operators';
 import { DeviceService, RequestService } from './shared/services';
 
 @Component({
@@ -24,14 +26,24 @@ export class AppComponent {
     private readonly device: DeviceService,
     // eslint-disable-next-line @typescript-eslint/ban-types
     @Inject(PLATFORM_ID) readonly platformId: Object,
+    @Inject(DOCUMENT) readonly document: Document,
     private readonly tracking: TrackingService,
     private readonly renderer: Renderer2,
+    private readonly update: SwUpdate,
   ) {
     if (isPlatformBrowser(platformId)) {
       this.device.setDeviceWidth();
       this.tracking.buildHeadScript(this.renderer);
       this.tracking.buildBodyScript(this.renderer);
     }
+    this.update.available.pipe(take(1)).subscribe(async () => {
+      try {
+        await update.activateUpdate();
+        this.document.location.reload();
+      } catch (err) {
+        console.error('swUpdate error', err);
+      }
+    });
   }
 
   onResize(): void {
